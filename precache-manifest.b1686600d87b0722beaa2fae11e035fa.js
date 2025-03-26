903fe9c4dc1fefad8bde87fea083ffce85525707
Ron <ronozp4@gmail.com>
2025-03-26 23:36:49 +0200

    Updates

---
+++ precache-manifest.b1686600d87b0722beaa2fae11e035fa.js
@@ -0,0 +1,394 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7ba435c2b3d71418eecf05dbe60540c",
+    "url": "/noyaharonson/index.html"
+  },
+  {
+    "revision": "bc2a9ca1118773bded17",
+    "url": "/noyaharonson/static/css/main.e80f8fc9.chunk.css"
+  },
+  {
+    "revision": "ae9be6385b31fdfb436d",
+    "url": "/noyaharonson/static/js/2.b512331f.chunk.js"
+  },
+  {
+    "revision": "bc2a9ca1118773bded17",
+    "url": "/noyaharonson/static/js/main.550bac6e.chunk.js"
+  },
+  {
+    "revision": "b1079d9a8b5a110df660",
+    "url": "/noyaharonson/static/js/runtime-main.aef4472d.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/noyaharonson/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "4457817ac2b9993c65e81aa05828fe9c",
+    "url": "/noyaharonson/static/media/GoogleSans-Bold.4457817a.ttf"
+  },
+  {
+    "revision": "90773b6158663ab0fe78b32680733677",
+    "url": "/noyaharonson/static/media/GoogleSans-BoldItalic.90773b61.ttf"
+  },
+  {
+    "revision": "0ecddcdeccb7761ce899aa9ad9f0ac3f",
+    "url": "/noyaharonson/static/media/GoogleSans-Italic.0ecddcde.ttf"
+  },
+  {
+    "revision": "8d57e4014b18edef070d285746485115",
+    "url": "/noyaharonson/static/media/GoogleSans-Medium.8d57e401.ttf"
+  },
+  {
+    "revision": "8fd3737925e83c87d78a13700ccf9a62",
+    "url": "/noyaharonson/static/media/GoogleSans-MediumItalic.8fd37379.ttf"
+  },
+  {
+    "revision": "b5c77a6aed75cdad9489effd0d5ea411",
+    "url": "/noyaharonson/static/media/GoogleSans-Regular.b5c77a6a.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/noyaharonson/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "493f206254be461836fada52cf56b258",
+    "url": "/noyaharonson/static/media/Rubik-Bold.493f2062.ttf"
+  },
+  {
+    "revision": "6840830e56060443e1cfda346bcc4189",
+    "url": "/noyaharonson/static/media/Rubik-Regular.6840830e.ttf"
+  },
+  {
+    "revision": "c42bbfcc63c8415eaabf6809d5f43d19",
+    "url": "/noyaharonson/static/media/aboutnoy.c42bbfcc.jpg"
+  },
+  {
+    "revision": "16559172ca1d4d552390b48f19e102ba",
+    "url": "/noyaharonson/static/media/address_image.16559172.svg"
+  },
+  {
+    "revision": "eaf02c2d0158b6b4b8c2a0a0cf830a04",
+    "url": "/noyaharonson/static/media/animated_ashutosh.eaf02c2d.png"
+  },
+  {
+    "revision": "dcd6dee2bd7458a4a576dc131506fd83",
+    "url": "/noyaharonson/static/media/animated_logo.dcd6dee2.svg"
+  },
+  {
+    "revision": "1f047bd39f93d1d2031523ea16c48d7d",
+    "url": "/noyaharonson/static/media/anotherNoy.1f047bd3.jpg"
+  },
+  {
+    "revision": "1e143438f89430b021c74d48d84df058",
+    "url": "/noyaharonson/static/media/anotherNoy2.1e143438.png"
+  },
+  {
+    "revision": "5d3a29da6523a49b1519a0cb7c03f466",
+    "url": "/noyaharonson/static/media/anotherNoy3.5d3a29da.jpg"
+  },
+  {
+    "revision": "25e7ef5a4d214b7dc1f4d2e65783b6b9",
+    "url": "/noyaharonson/static/media/balance.25e7ef5a.png"
+  },
+  {
+    "revision": "058eca4cbdc03a1dad9a6bb943ac7831",
+    "url": "/noyaharonson/static/media/blogs_image.058eca4c.svg"
+  },
+  {
+    "revision": "cacbc42b661f39344af81c631d8d36a9",
+    "url": "/noyaharonson/static/media/cloud_infrastructure.cacbc42b.svg"
+  },
+  {
+    "revision": "fee92c18b1cfbd1faa704c51047f113d",
+    "url": "/noyaharonson/static/media/codeInLogo.fee92c18.png"
+  },
+  {
+    "revision": "5f915405511f649dad433a6db9adbff4",
+    "url": "/noyaharonson/static/media/contactMail.5f915405.png"
+  },
+  {
+    "revision": "ebf35d7f33ebbd4a4c6cff7e66e21fdb",
+    "url": "/noyaharonson/static/media/data_science.ebf35d7f.svg"
+  },
+  {
+    "revision": "75a7107b7739a46950f66de29779eb52",
+    "url": "/noyaharonson/static/media/deeplearning_ai_logo.75a7107b.png"
+  },
+  {
+    "revision": "3e23ca14e7a7c8a7b63d255466749000",
+    "url": "/noyaharonson/static/media/developerActivity.3e23ca14.svg"
+  },
+  {
+    "revision": "63f707f224c8a842ac928c36e3be879a",
+    "url": "/noyaharonson/static/media/dsc_logo.63f707f2.png"
+  },
+  {
+    "revision": "3bab7a9dc4324f1b2364d25a47fcfcf0",
+    "url": "/noyaharonson/static/media/education.3bab7a9d.svg"
+  },
+  {
+    "revision": "a00ede3f70759480e81f14f203edaa51",
+    "url": "/noyaharonson/static/media/experience.a00ede3f.svg"
+  },
+  {
+    "revision": "0017cc6de3f72f67753efa37bb796fbd",
+    "url": "/noyaharonson/static/media/fa-brands-400.0017cc6d.svg"
+  },
+  {
+    "revision": "06147b6cd88c7346cecd1edd060cd5de",
+    "url": "/noyaharonson/static/media/fa-brands-400.06147b6c.ttf"
+  },
+  {
+    "revision": "5063b105c7646c8043d58c5289f02cca",
+    "url": "/noyaharonson/static/media/fa-brands-400.5063b105.eot"
+  },
+  {
+    "revision": "c5e0f14f88a828261ba01558ce2bf26f",
+    "url": "/noyaharonson/static/media/fa-brands-400.c5e0f14f.woff"
+  },
+  {
+    "revision": "cccc9d29470e879e40eb70249d9a2705",
+    "url": "/noyaharonson/static/media/fa-brands-400.cccc9d29.woff2"
+  },
+  {
+    "revision": "65b286af947c0d982ca01b40e1fcab38",
+    "url": "/noyaharonson/static/media/fa-regular-400.65b286af.ttf"
+  },
+  {
+    "revision": "c1a866ec0e04a5e1915b41fcf261457c",
+    "url": "/noyaharonson/static/media/fa-regular-400.c1a866ec.eot"
+  },
+  {
+    "revision": "c4f508e7c4f01a9eeba7f08155cde04e",
+    "url": "/noyaharonson/static/media/fa-regular-400.c4f508e7.woff"
+  },
+  {
+    "revision": "e647ddbb6d95cb01deac07ffafb2a085",
+    "url": "/noyaharonson/static/media/fa-regular-400.e647ddbb.svg"
+  },
+  {
+    "revision": "f5f2566b93e89391da4db79462b8078b",
+    "url": "/noyaharonson/static/media/fa-regular-400.f5f2566b.woff2"
+  },
+  {
+    "revision": "0bff33a5fd7ec390235476b4859747a0",
+    "url": "/noyaharonson/static/media/fa-solid-900.0bff33a5.ttf"
+  },
+  {
+    "revision": "333bae208dc363746961b234ff6c2500",
+    "url": "/noyaharonson/static/media/fa-solid-900.333bae20.woff"
+  },
+  {
+    "revision": "424c59fb1df79fcc3d0a41909c0192d2",
+    "url": "/noyaharonson/static/media/fa-solid-900.424c59fb.svg"
+  },
+  {
+    "revision": "44d537ab79f921fde5a28b2c1636f397",
+    "url": "/noyaharonson/static/media/fa-solid-900.44d537ab.woff2"
+  },
+  {
+    "revision": "8e4a6dcc692b3887f9f542cd6894d6d4",
+    "url": "/noyaharonson/static/media/fa-solid-900.8e4a6dcc.eot"
+  },
+  {
+    "revision": "ec5e017c0bc52d7a9738f091662879be",
+    "url": "/noyaharonson/static/media/family.ec5e017c.png"
+  },
+  {
+    "revision": "2936ce95f550e3feef7230ea543a4b60",
+    "url": "/noyaharonson/static/media/feelingProud.2936ce95.svg"
+  },
+  {
+    "revision": "fce6080726685cabdb4f75a0d8018325",
+    "url": "/noyaharonson/static/media/fullstack.fce60807.svg"
+  },
+  {
+    "revision": "0ee7e8c9444cdfefc10f6d86e18b9b5b",
+    "url": "/noyaharonson/static/media/gcp_logo.0ee7e8c9.png"
+  },
+  {
+    "revision": "6eeb2e810d0fd9f3ca2dcd72de228e68",
+    "url": "/noyaharonson/static/media/github_logo.6eeb2e81.png"
+  },
+  {
+    "revision": "0c8a4c114ad00f7854e91550379b9b91",
+    "url": "/noyaharonson/static/media/googleAssistant.0c8a4c11.jpg"
+  },
+  {
+    "revision": "a6addc3fb6605480614b992ede8cd434",
+    "url": "/noyaharonson/static/media/googleAssistant.a6addc3f.svg"
+  },
+  {
+    "revision": "9b8c7cd9053b9f39eaef7cc5ca72abe6",
+    "url": "/noyaharonson/static/media/handshake (1).9b8c7cd9.png"
+  },
+  {
+    "revision": "c37c6064e731bb8de567d5fff4e1ad03",
+    "url": "/noyaharonson/static/media/handshake.c37c6064.png"
+  },
+  {
+    "revision": "25af6a33e21da769b730463db50772b3",
+    "url": "/noyaharonson/static/media/home (1).25af6a33.png"
+  },
+  {
+    "revision": "d3fccbe1db76b96f4f320d9c0f7da30c",
+    "url": "/noyaharonson/static/media/ibm_logo.d3fccbe1.png"
+  },
+  {
+    "revision": "571a8584e69ecfbe326c8b8385fbbf54",
+    "url": "/noyaharonson/static/media/iiitk_logo.571a8584.png"
+  },
+  {
+    "revision": "29152cf756a1facead5393c32e373371",
+    "url": "/noyaharonson/static/media/intel_logo.29152cf7.jpg"
+  },
+  {
+    "revision": "60ed0bd3f8b99ac1d29e74575f804aaa",
+    "url": "/noyaharonson/static/media/iu_logo.60ed0bd3.png"
+  },
+  {
+    "revision": "5b4a6449747ec61a6d9aa874f2a9ceba",
+    "url": "/noyaharonson/static/media/jsFramework.5b4a6449.svg"
+  },
+  {
+    "revision": "cfe6964f418b925b4998f2f893bfeb23",
+    "url": "/noyaharonson/static/media/lawyer.cfe6964f.jpg"
+  },
+  {
+    "revision": "6dccdb96f959cff943b2a8b6526a2387",
+    "url": "/noyaharonson/static/media/lawyer2.6dccdb96.png"
+  },
+  {
+    "revision": "c597fbded5de799dfb9abc3fc4f1c26f",
+    "url": "/noyaharonson/static/media/legato_logo.c597fbde.png"
+  },
+  {
+    "revision": "72bd1de96d3a555db393cad2c4531057",
+    "url": "/noyaharonson/static/media/mainNoy.72bd1de9.png"
+  },
+  {
+    "revision": "83ac24fb968a474c6a63f7a4e9a8f62c",
+    "url": "/noyaharonson/static/media/manOnTable.83ac24fb.svg"
+  },
+  {
+    "revision": "069938a84edd6ebf383c318e9feb5d4c",
+    "url": "/noyaharonson/static/media/muffito_logo.069938a8.png"
+  },
+  {
+    "revision": "9557856729dbcc9b4644de3b6b3dbf76",
+    "url": "/noyaharonson/static/media/noyMain.95578567.png"
+  },
+  {
+    "revision": "fc1da836532bae6fcb2712132ddcfdbf",
+    "url": "/noyaharonson/static/media/noy_main-removebg-preview.fc1da836.png"
+  },
+  {
+    "revision": "fbe3288138747e0e8233a2621e6b7c41",
+    "url": "/noyaharonson/static/media/noy_main.fbe32881.jpeg"
+  },
+  {
+    "revision": "95a12a2b8c3149cce2175679755c4b4d",
+    "url": "/noyaharonson/static/media/nptel_logo.95a12a2b.png"
+  },
+  {
+    "revision": "414dfbd98516b39255a3e22ed90b3f7d",
+    "url": "/noyaharonson/static/media/portfolio.414dfbd9.gif"
+  },
+  {
+    "revision": "5faf790badc18fa9030f94836ece1df1",
+    "url": "/noyaharonson/static/media/programmer.5faf790b.svg"
+  },
+  {
+    "revision": "b8ba948796d7ab532673c5ed2f315e74",
+    "url": "/noyaharonson/static/media/projects_image.b8ba9487.svg"
+  },
+  {
+    "revision": "71408db71465afd742233e4a96f06526",
+    "url": "/noyaharonson/static/media/pwa.71408db7.png"
+  },
+  {
+    "revision": "db8579dea2e2b39dbd0d94797bfd2e6e",
+    "url": "/noyaharonson/static/media/reshot-icon-code-of-conduct-D4FHU82MRV.db8579de.svg"
+  },
+  {
+    "revision": "10d670725e9b0036910dd6fa1d9074bc",
+    "url": "/noyaharonson/static/media/reshot-icon-copyright-expiry-D7WHSGQVKF.10d67072.svg"
+  },
+  {
+    "revision": "d5406a08b9b0ecfb43cc67613437dee0",
+    "url": "/noyaharonson/static/media/reshot-icon-court-9BDTLH3XNP.d5406a08.svg"
+  },
+  {
+    "revision": "8a5291b5f82b8c2de02ac421a18d4fe5",
+    "url": "/noyaharonson/static/media/reshot-icon-court-EHKZWYMJQ9.8a5291b5.svg"
+  },
+  {
+    "revision": "923471a6f2ba65e6546789496da1e805",
+    "url": "/noyaharonson/static/media/reshot-icon-judge-6RSZ3DPKHL.923471a6.svg"
+  },
+  {
+    "revision": "56166bc727bd6b338f099f916304c349",
+    "url": "/noyaharonson/static/media/reshot-icon-judge-LYHN7WC53Z.56166bc7.svg"
+  },
+  {
+    "revision": "bce9b969d0743f9091500d1cc8036f54",
+    "url": "/noyaharonson/static/media/reshot-icon-judge-MUS7L6DRW4.bce9b969.svg"
+  },
+  {
+    "revision": "bd8a68ecf1949ad8c5fc73ea252fa948",
+    "url": "/noyaharonson/static/media/reshot-icon-judge-RZU5JY9376.bd8a68ec.svg"
+  },
+  {
+    "revision": "58225b29fed583658e64a3178bb599f4",
+    "url": "/noyaharonson/static/media/reshot-icon-judge-Y68N7XFW2P.58225b29.svg"
+  },
+  {
+    "revision": "6e2d24e4f9d76c46c2c86e9e5844fdd0",
+    "url": "/noyaharonson/static/media/reshot-icon-justice-VMT5RU6N93.6e2d24e4.svg"
+  },
+  {
+    "revision": "4130e10304c0995dcb76a1cc77210a34",
+    "url": "/noyaharonson/static/media/reshot-icon-justice-WB239THRL8.4130e103.svg"
+  },
+  {
+    "revision": "339f3da7aa74bac5f380b89b6a11fff5",
+    "url": "/noyaharonson/static/media/reshot-icon-law-7Y6CXTKZWJ.339f3da7.svg"
+  },
+  {
+    "revision": "4e2f4cde5ddde15ee2514c0a488f7a10",
+    "url": "/noyaharonson/static/media/reshot-icon-law-EUD23AVS5T.4e2f4cde.svg"
+  },
+  {
+    "revision": "a7f5188176df74d7c90589636f728381",
+    "url": "/noyaharonson/static/media/reshot-icon-law-EWT36PFMG9.a7f51881.svg"
+  },
+  {
+    "revision": "fb604d8e6998106af9e850c9eb5131b2",
+    "url": "/noyaharonson/static/media/reshot-icon-lawyer-P3876TUEZQ.fb604d8e.svg"
+  },
+  {
+    "revision": "cff6ead1c555bab9f4b4db4bcc0aed7e",
+    "url": "/noyaharonson/static/media/reshot-icon-legal-mail-YRA5CENWHQ.cff6ead1.svg"
+  },
+  {
+    "revision": "2c497738be3831492aeb6bdc43f19154",
+    "url": "/noyaharonson/static/media/stanford_logo.2c497738.png"
+  },
+  {
+    "revision": "6ac91b161c2c9934a44f72d75d18142d",
+    "url": "/noyaharonson/static/media/talksCardBack.6ac91b16.svg"
+  },
+  {
+    "revision": "3fa5424232370e3d049c9555d9c440a4",
+    "url": "/noyaharonson/static/media/talksCardBackAlt.3fa54242.svg"
+  },
+  {
+    "revision": "0993e3246bf5146154391a0afef79b03",
+    "url": "/noyaharonson/static/media/tiktok_logo.0993e324.png"
+  },
+  {
+    "revision": "d63630893eb8cb64af65f395d393c481",
+    "url": "/noyaharonson/static/media/ui_ux_design.d6363089.svg"
+  }
+]);
